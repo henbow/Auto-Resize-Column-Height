4160c566d67fbd1122430452227f7cf966c70c8d
Hendro Wibowo <hendro@tiwule.net>
2012-11-12 23:54:56 +0700

    add target element option for more flexibility

--- src/jquery.ikutan.js
+++ src/jquery.ikutan.js
@@ -1,20 +1,26 @@
 /**
  * jquery.ikutan.js
  * Auto height div column
- * @author Hendro Wibowo (hendrothemail@gmail.com)
+ * @author Hendro Wibowo (hendro@tiwule.net)
  * 
  */
 
 if (jQuery) (function($) {
 	$.extend($.fn, {
-		ikutan: function(opt){			
+		ikutan: function(opt){
+			var opts = {
+				targetElem: opt.target == undefined ? 'div' : opt.target				
+			};
 			var self = $(this);
-			var heights = $(this).children().map(function (){
-		        return $(this).height();
-		    }).get(),
-		    maxHeight = Math.max.apply(null, heights);		    
+			var heights = $(this).children(opts.targetElem).map(function (){ return $(this).height(); }).get();
+		    var maxHeight = Math.max.apply(null, heights);		    
 		    $(this).children().height(maxHeight);
 		    $(this).height(maxHeight);
 		}
 	});
 })(jQuery);	
+
+/**
+ * End of file jquery.ikutan.js
+ *  
+ */
